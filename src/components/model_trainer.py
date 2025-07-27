2131cccf1f3725c39d9c2328ede6fc052fc5c3d1
a-leena <aleenabiju09@gmail.com>
2025-07-27 15:45:46 +0530

    added hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -19,9 +19,60 @@ from sklearn.metrics import r2_score
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
 
+@dataclass
+class ModelEvaluatorConfig:
+    models = {
+            "Linear Regression": LinearRegression(),
+            "K-Neighbors Regression": KNeighborsRegressor(),
+            "Decision Tree": DecisionTreeRegressor(),
+            "Random Forest Regression": RandomForestRegressor(),
+            "XGB Regression": XGBRegressor(),
+            "CatBoosting Regression": CatBoostRegressor(verbose=False),
+            "AdaBoost Regression": AdaBoostRegressor(),
+            "Gradient Boosting Regression": GradientBoostingRegressor()
+    }
+    params = {
+        "Linear Regression": {
+            "fit_intercept" : [True, False]
+        },
+        "K-Neighbors Regression": {
+            "n_neighbors": [3,5,7,10,15,20],
+            "weights": ['uniform', 'distance'],
+            "algorithm": ['auto', 'ball_tree', 'kd_tree', 'brute']
+        },
+        "Decision Tree": {
+            "max_depth": [None, 1, 3, 5, 10, 15, 20],
+            "min_samples_leaf": [1, 2, 3, 5, 7, 10]
+        },
+        "Random Forest Regression": {
+            "max_features": ['sqrt', 'log2', None],
+            "n_estimators": [10, 20, 50, 75, 100, 500, 1000]
+        },
+        "XGB Regression": {
+            "learning_rate": [0.001, 0.01, 0.05, 0.1, 0.5],
+            "n_estimators": [10, 20, 50, 75, 100, 500, 1000]
+        },
+        "CatBoosting Regression": {
+            "iterations": [10, 25, 50, 75, 100],
+            "learning_rate": [0.001, 0.01, 0.05, 0.1, 0.5]
+        },
+        "AdaBoost Regression": {
+            "loss": ['linear', 'square', 'exponential'],
+            "learning_rate": [0.001, 0.01, 0.05, 0.1, 0.5],
+            "n_estimators": [10, 20, 50, 75, 100, 500, 1000]
+        },
+        "Gradient Boosting Regression": {
+            "learning_rate": [0.001, 0.01, 0.05, 0.1, 0.5],
+            "criterion": ['friedman_mse', 'squared_error'],
+            "n_estimators": [10, 20, 50, 75, 100, 500, 1000],
+            "min_samples_leaf": [1, 2, 3, 5, 7, 10]
+        }
+    }
+
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluator_config = ModelEvaluatorConfig()
 
     def initiate_model_trainer(self, train_array, test_array):
         try:
@@ -31,30 +82,23 @@ class ModelTrainer:
                                                 test_array[:,:-1], 
                                                 test_array[:,-1])
             
-            models = {
-                    "Linear Regression": LinearRegression(),
-                    "K-Neighbors Regression": KNeighborsRegressor(),
-                    "Decision Tree": DecisionTreeRegressor(),
-                    "Random Forest Regression": RandomForestRegressor(),
-                    "XGB Regression": XGBRegressor(),
-                    "CatBoosting Regression": CatBoostRegressor(verbose=False),
-                    "AdaBoost Regression": AdaBoostRegressor(),
-                    "Gradient Boosting Regression": GradientBoostingRegressor()
-                    }
+            model_r2_report, model_param_report = evaluate_models(
+                X_train=X_train, y_train=y_train, 
+                X_test=X_test, y_test=y_test, 
+                models=self.model_evaluator_config.models,
+                params=self.model_evaluator_config.params)
             
-            model_report = evaluate_models(X_train=X_train, y_train=y_train, 
-                                          X_test=X_test, y_test=y_test, 
-                                          models=models)
+            logging.info("All models trained and evaluated.")
             
-            best_model_score = max(sorted(model_report.values()))
+            best_model_score = max(sorted(model_r2_report.values()))
 
-            # setting a threshold for the scores (we can add hyperparameter tuning or something here then)
+            # setting a threshold for the scores
             if best_model_score < 0.6:
                 raise CustomException("No best model found.", sys)
 
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
-
-            best_model = models[best_model_name]
+            best_model_name = list(model_r2_report.keys())[list(model_r2_report.values()).index(best_model_score)]
+            best_model_params = model_param_report[best_model_name]
+            best_model = self.model_evaluator_config.models[best_model_name]
             logging.info("Best model found.")
 
             save_object(
@@ -63,7 +107,7 @@ class ModelTrainer:
             )
             logging.info("Best model saved.")
 
-            return best_model_score
+            return best_model_name, best_model_score, best_model_params
 
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 def save_object(file_path, object):
     try:
@@ -18,18 +19,46 @@ def save_object(file_path, object):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
-        report = {}
+        r2_report = {}
+        params_report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
+            param = list(params.values())[i]
+
+            # random_search = RandomizedSearchCV(
+            #     estimator=model,
+            #     param_distributions=param,
+            #     n_iter=20,
+            #     cv=5,
+            #     scoring='neg_mean_squared_error',
+            #     random_state=42,
+            #     n_jobs=-1
+            # )
+            # random_search.fit(X_train, y_train)
+            # best_model = random_search.best_estimator_
+            # best_params = random_search.best_params_
+
+            grid_search = GridSearchCV(
+                estimator=model,
+                param_grid=param,
+                cv=5,
+                scoring='neg_mean_squared_error',
+                n_jobs=-1
+            )
+            grid_search.fit(X_train, y_train)
+            best_model = grid_search.best_estimator_
+            best_params = grid_search.best_params_
+
+            y_pred = best_model.predict(X_test)
             r2 = r2_score(y_test, y_pred)
-            report[list(models.keys())[i]] = r2
+
+            r2_report[list(models.keys())[i]] = r2
+            params_report[list(models.keys())[i]] = best_params
         
-        return report
+        return r2_report, params_report
      
     except Exception as e:
         raise CustomException(e, sys)
